8b9bce08a3c0552af8c7b148b1a8f9f2cd1974bd
DESKTOP-LKJELH9\AllBreaker <gabrielmontano.allbreaker@gmail.com>
2022-08-24 12:55:30 -0500

    web para el viernes con modelo simple y animacion sencilla

--- Modelos_AR_AWS/conexion.js
+++ Modelos_AR_AWS/conexion.js
@@ -2,22 +2,24 @@ var mysql = require('mysql');
 var AWS = require('aws-sdk');
 
 var conexion = mysql.createConnection({
-host: 'dbpruebasmodelwebar.ccnilygdxq9g.us-east-1.rds.amazonaws.com',
-database: 'modelos_web',
+host: 'POINT_OF_ACCESS',
+database: 'DATABASE',
 user: 'admin',
-password: '	data-modelweb'
+password: 'CONTRA_POINT_OF_ACCESS'
 });
+
+
 
 conexion.connect(function(error){
   if (error) {
     throw error;
   }else{
     console.log('Conexion Exitosa');
-    respuesta.writeHead(500, { 'Content-Type': 'text/plain' })
-    respuesta.write('cargo exitosamente la DB')
-    respuesta.end()
+    bl = true;
   }
 });
+
+
 
 conexion.end();
 
