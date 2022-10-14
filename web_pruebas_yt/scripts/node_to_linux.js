cbdf6cd1a8ca1327845135a4c8668bc053531c39
DESKTOP-LKJELH9\AllBreaker <gabrielmontano.allbreaker@gmail.com>
2022-10-14 09:57:32 -0500

    prueba de ejecucion de linux desde nodejs

---
+++ web_pruebas_yt/scripts/node_to_linux.js
@@ -0,0 +1,15 @@
+import * as child from 'child_process';
+
+
+
+child.exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
