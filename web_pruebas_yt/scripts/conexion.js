ff84534f39e22adcb62710a46f36977ddaf13f27
DESKTOP-LKJELH9\AllBreaker <gabrielmontano.allbreaker@gmail.com>
2022-10-20 13:59:43 -0500

    se borro async function

--- web_pruebas_yt/scripts/conexion.js
+++ web_pruebas_yt/scripts/conexion.js
@@ -7,11 +7,11 @@ var conexion = mysql.createConnection({
   host: process.env.POINT_OF_ACCESS,
   database: process.env.DB,
   user: process.env.USERDB,
-  password:process.env.PASS 
+  password: ''
 });
+//process.env.PASS
 
-
-let conectando = conexion.connect(async function (error) {
+let conectando = conexion.connect( function (error) {
   if (error) {
     throw error;
   } else {
